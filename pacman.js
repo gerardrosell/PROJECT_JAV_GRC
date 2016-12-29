7558afcca8cb09e04c2268d2b6829368d2e02a52
gerardrosell <gerard.rosell.cardus@gmail.com>
2016-12-29 17:13:32 +0100

    pacman es mou amb animacions

--- pacman.js
+++ pacman.js
@@ -1,19 +1,29 @@
-var pacman = new Phaser.Game(540, 528, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create });
+var pacman = new Phaser.Game(540, 528, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update });
 
 function preload() {
 
     pacman.load.tilemap('fondo', 'public/Pacman.json', null, Phaser.Tilemap.TILED_JSON);
     pacman.load.image('tiles', 'public/pacManTiles.png');
+    pacman.load.spritesheet('cc', 'public/ChomperSprites16x16.png', 16, 16, 14*4);
 }
 
 var map;
 var layer;
 var p;
+var player;
+var posx=10;
+var posy=15;
+
+var upKey;
+var downKey;
+var leftKey;
+var rightKey;
 
 function create() {
 
     pacman.stage.backgroundColor = '#CCCCCC';
-
+    pacman.physics.startSystem(Phaser.Physics.ARCADE);
+    
     //  The 'mario' key here is the Loader key given in game.load.tilemap
     map = pacman.add.tilemap('fondo');
 
@@ -27,6 +37,53 @@ function create() {
 
     //  This resizes the game world to match the layer dimensions
     layer.resizeWorld();
+
+    //afegeix sprite a la posició 0,0 amb la imatge cc
+    player = pacman.add.sprite(posx*16,posy*16, 'cc');
+
+    player.animations.add('left', [39, 38], 10, true);
+    player.animations.add('right', [11,10], 10, true);
+    player.animations.add('up', [53,52], 10, true);
+    player.animations.add('down', [25,24], 10, true);
+
+    pacman.physics.arcade.enable(player);
+
+    upKey = pacman.input.keyboard.addKey(Phaser.Keyboard.UP);
+    downKey = pacman.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+    leftKey = pacman.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+    rightKey = pacman.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+    
+
+
+
+}
+
+function update(){
+        if (leftKey.isDown) {
+            //  Move to the left
+            player.body.position.x-=1;
+            player.animations.play('left');
+        }
+        else if (rightKey.isDown)
+        {
+            //  Move to the right
+            player.body.position.x+=1;
+            player.animations.play('right');
+        }
+        else if (upKey.isDown)
+        {
+            //  Move to the right
+            player.body.position.y-=1;
+            player.animations.play('up');
+        }
+        else if (downKey.isDown)
+        {
+            //  Move to the right
+            player.body.position.y+=1;
+            player.animations.play('down');
+        }
+        player.set
+        
 }
 
 function actionOnClick () {
